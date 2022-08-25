931ed0127565d0d766910195327dea8b2ff8ed26
par3sh <parkamat007@gmail.com>
2022-08-25 10:08:53 +0530

    commiting code

--- controller.py
+++ controller.py
@@ -13,34 +13,36 @@ led_3=board.get_pin('d:11:o')
 led_4=board.get_pin('d:10:o')
 led_5=board.get_pin('d:9:o')
 
-led_1.write(0)
-k = input("Enter hours 'H', minutes 'm', seconds's'")
-
-a  = float(input("Enter time to turn on: "))
-k.lower()
-
-if k == str('h'):
-    a = a*3600
-
-elif k == str('m'):
-    a = a*60
-
-else :
-    k = k*1
-
-while a > 0:
-    led_1.write(1)
-    led_2.write(1)
-    led_3.write(1)
-    led_4.write(1)
-    time.sleep(a)
+def charger():
     led_1.write(0)
-    led_2.write(0)
-    led_3.write(0)
-    led_4.write(0)
-    time.sleep(1)
-    break
-'''
+    k = input("Enter hours 'H', minutes 'm', seconds's'")
+
+    a  = float(input("Enter time to turn on: "))
+    k.lower()
+
+    if k == str('h'):
+        a = a*3600
+
+    elif k == str('m'):
+        a = a*60
+
+    else :
+        k = k*1
+
+    while a > 0:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        time.sleep(a)
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        time.sleep(1)
+        break
+
+
 def led(total):
     if total==0:
         led_1.write(0)
@@ -79,4 +81,13 @@ def led(total):
         led_3.write(1)
         led_4.write(1)
         led_5.write(1)
-'''
+
+
+z = input("Time based 'T' or Gesture based 'G'")
+z.lower()
+
+
+if z == str('t'):
+    charger()
+else:
+    led()
